9fd61194c6936be084a7d8adc98d487c8b9bf757
Danny Versantvoort <danny.versantvoort@asml.com>
2023-08-15 11:45:10 +0200

    Introduced Su Starter to SU applications added tests and fixed bean injection

--- config-service/src/test/java/nl/danman/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/nl/danman/configservice/ConfigServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package nl.danman.configservice;
 
+import nl.danman.su.spring.boot.starter.service.StartupService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class ConfigServiceApplicationTests {
 
+    @SpyBean
+    private StartupService startupServiceSpy;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void startUpService_isCalledAtStartup() {
+        verify(startupServiceSpy).atStartUp();
     }
 
 }
--- demo-service/src/main/java/nl/danman/demoservice/DemoServiceApplication.java
+++ demo-service/src/main/java/nl/danman/demoservice/DemoServiceApplication.java
@@ -1,6 +1,7 @@
 package nl.danman.demoservice;
 
 import nl.danman.su.spring.boot.starter.service.StartupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class DemoServiceApplication implements CommandLineRunner {
 
+    @Autowired
     private StartupService startupService;
 
     public static void main(String[] args) {
